fa9a9f73cc08ddbad00003544d0e07b4a2edb429
Nikolai Bozhenov <nikolai.bozhenov@gmail.com>
2016-03-30 18:56:50 +0300

    etags-select: Enable TAB and RETURN in terminal

--- emacs/init/tags.el
+++ emacs/init/tags.el
@@ -3,7 +3,15 @@
 ;; этот список функции ищут совсем неразборчиво.
 
 (load-file "~/z/scratchpad/emacs/init/etags-select.el")
+
+(defun etags-select-tab-pressed ()
+  (interactive)
+  (etags-select-goto-tag 4 t))
+
 (define-key etags-select-mode-map [tab] (lambda () (interactive) (etags-select-goto-tag 4 t)))
+(define-key etags-select-mode-map (kbd "TAB") (lambda () (interactive) (etags-select-goto-tag 4 t)))
+(define-key etags-select-mode-map (kbd "C-i") (lambda () (interactive) (etags-select-goto-tag 4 t)))
+(define-key etags-select-mode-map (kbd "C-m") 'etags-select-goto-tag)
 
 ;; (defadvice etags-select-find-tag-at-point (before y-push-mark-onto-ring)
 ;;   (ring-insert find-tag-marker-ring (point-marker)))
