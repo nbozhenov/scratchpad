32091496a6811cd4a16487578e50dad37689d44b
Nikolai Bozhenov <nikolai.bozhenov@gmail.com>
2018-01-02 23:11:44 +0100

    New GTD schema

--- emacs/init/org.el
+++ emacs/init/org.el
@@ -38,7 +38,7 @@
 ;; agenda
 (if (file-exists-p yorg-path)
   (setq org-agenda-files (directory-files yorg-path t "\\.todo\\'")))
-(setq org-agenda-todo-ignore-scheduled t)
+(setq org-agenda-todo-ignore-scheduled nil)
 (setq org-agenda-skip-scheduled-if-done t)
 (setq org-agenda-start-on-weekday nil)
 (setq org-agenda-restore-windows-after-quit t)
@@ -64,28 +64,21 @@
 (setq org-startup-indented t)
 ;(setq org-startup-with-inline-images t)
 
-;; TODO keywords
 (setq org-todo-keywords
-      '((sequence "FIXME"          ;; <- not a TODO item, just a remark that this area hasn't been
-                                   ;;    fully explored
-                  "NOTE(n)"        ;; <- nota bene (заметка), тонкое место, к-рое следует учесть
-                                   ;;    при выполнении родительской задачи
-                  "TODO(t)"        ;; <- текущая задача
-                  "CURR(c)"        ;; <- work in progress
-                  "WAIT(w)"        ;; <- к выполнению задачи можно будет приступить
-                                   ;;    после выполнения некоторых условий
-                                   ;;    (в эту категорию попадают заблокированные задачи)
-              "|" "DONE(d)"
-                  "SKIP(s)")))
+      '((sequence "PROJECT(p)" "|" "CLOSED(l)" "CANCEL(x)")
+        (sequence "WAIT(w)" "TODO(t)" "|" "DONE(d)" "SKIP(s)")
+        (sequence "FIXME(m)" "|" "FIXED(f)")))
 
 (setq org-todo-keyword-faces
-      '(("FIXME" . "dark orange")
-        ; ("NOTE" . "dark red")
-        ("NOTE" . "orange red")
-        ("TODO" . "firebrick")
-        ("CURR" . "purple")
+      '(("FIXME" . "red4")
+        ("FIXED" . "#083708")
         ("WAIT" . "chocolate4")
-        ("SKIP" . (:foreground "DeepSkyBlue3" :weight bold))))
+        ("TODO" . "firebrick")
+        ("DONE" . "dark green")
+        ("SKIP" . "#0052c0")
+        ("PROJECT" . "purple")
+        ("CLOSED" . "dark green")
+        ("CANCEL" . "#404070")))
 
 (setq org-list-description-max-indent 5)
 
@@ -94,17 +87,150 @@
  (lambda ()
    (define-key org-agenda-mode-map (kbd "C-C C-p") 'org-agenda-priority)))
 
+(setq org-tag-alist '((:startgroup . nil)
+                      ("office" . ?o)
+                      ("home" . ?h)
+                      ("sveta" . ?s)
+                      (:endgroup . nil)))
+
+
+(defun my-org-agenda-skip-check (type)
+  (let* ((tags (org-get-tags-at)))
+    (cond
+     ;; "all" : skip nothing
+     ((string= type "all")
+      nil)
+     ;; "office" : skip all tagged entries (except ones explicitely tagged with :office:)
+     ((string= type "office")
+      (or (member-ignore-case "home" tags) (member-ignore-case "sveta" tags)))
+     ;; "home" : skip all entries unless they are tagged with :home:
+     ((string= type "home")
+      (not (member-ignore-case "home" tags)))
+     ;; "home" : skip all entries unless they are tagged with :sveta:
+     ((string= type "sveta")
+      (not (member-ignore-case "sveta" tags)))
+     (t
+      (error "Invalid agenda type")))))
+
+;; (macroexpand '(my-org-agenda-skip-macro "home" org-agenda-skip-subtree-if 'nottodo '("TODO" "WAIT")))
+;; =>
+;; (quote
+;;  (if (my-org-agenda-skip-check "home")
+;;      (org-agenda-skip-subtree-if (quote regexp) ".*")
+;;    (org-agenda-skip-subtree-if (quote nottodo) (quote ("TODO" "WAIT")))))
+(defmacro my-org-agenda-skip-macro (type func &rest conditions)
+  `(quote (if (my-org-agenda-skip-check ,type)
+              (,func 'regexp ".*")
+            ,(cons func conditions))))
+
+;; (macroexpand '(my-org-agenda-custom-template "xh" "My Home Agenda" "home"))
+;; =>
+;; ("xh" "My Home Agenda"
+;;  ((agenda ""
+;;           ((org-agenda-overriding-header "Upcoming Agenda")
+;;            (org-agenda-span 8)
+;;            (org-agenda-skip-function
+;;             (quote (if (my-org-agenda-skip-check "home")
+;;                        (org-agenda-skip-entry-if (quote regexp) ".*")
+;;                      (org-agenda-skip-entry-if (quote nottodo) (quote ("TODO" "WAIT"))))))))
+;;
+;;   (todo "PROJECT"
+;;         ((org-agenda-overriding-header "Active Projects")
+;;          (org-agenda-todo-ignore-scheduled (quote future))
+;;          (org-agenda-skip-function
+;;           (quote (if (my-org-agenda-skip-check "home")
+;;                      (org-agenda-skip-entry-if (quote regexp) ".*")
+;;                    (org-agenda-skip-entry-if (quote notregexp) ".*"))))))
+;;
+;;   (todo "PROJECT|TODO"
+;;         ((org-agenda-overriding-header "Incoming Tasks")
+;;          (org-agenda-todo-ignore-scheduled t)
+;;          (org-agenda-skip-function
+;;           (quote (if (my-org-agenda-skip-check "home")
+;;                      (org-agenda-skip-subtree-if (quote regexp) ".*")
+;;                    (org-agenda-skip-subtree-if (quote todo) (quote ("PROJECT"))))))))
+;;
+;;   (todo "PROJECT|WAIT"
+;;         ((org-agenda-overriding-header "Postponed Tasks")
+;;          (org-agenda-todo-ignore-scheduled t)
+;;          (org-agenda-skip-function
+;;           (quote (if (my-org-agenda-skip-check "home")
+;;                      (org-agenda-skip-subtree-if (quote regexp) ".*")
+;;                    (org-agenda-skip-subtree-if (quote todo) (quote ("PROJECT"))))))))))
+(defmacro my-org-agenda-custom-template (shortcut description type)
+  (list
+   shortcut
+   description
+   `((agenda ""
+             ((org-agenda-overriding-header "Upcoming Agenda")
+              (org-agenda-span 8)
+              (org-agenda-skip-function ,(macroexpand
+                                          `(my-org-agenda-skip-macro
+                                            ,type
+                                            org-agenda-skip-entry-if 'nottodo '("TODO" "WAIT"))))))
+
+     (todo "PROJECT"
+           ((org-agenda-overriding-header "Active Projects")
+            (org-agenda-todo-ignore-scheduled 'future)
+            (org-agenda-skip-function ,(macroexpand
+                                        `(my-org-agenda-skip-macro
+                                          ,type
+                                          ;; skip nothing regexp
+                                          org-agenda-skip-entry-if 'notregexp ".*")))))
+
+     (todo "PROJECT|TODO" ;; PROJECT is necessary in this list to feed it into skip function
+           ((org-agenda-overriding-header "Incoming Tasks")
+            (org-agenda-todo-ignore-scheduled t)
+            (org-agenda-skip-function ,(macroexpand
+                                        `(my-org-agenda-skip-macro
+                                          ,type
+                                          org-agenda-skip-subtree-if 'todo '("PROJECT"))))))
+
+     (todo "PROJECT|WAIT" ;; PROJECT is necessary in this list to feed it into skip function
+           ((org-agenda-overriding-header "Postponed Tasks")
+            (org-agenda-todo-ignore-scheduled t)
+            (org-agenda-skip-function ,(macroexpand
+                                        `(my-org-agenda-skip-macro
+                                          ,type
+                                          org-agenda-skip-subtree-if 'todo '("PROJECT"))))))
+     )))
+
+;; (("y" "My Agenda"
+;;   ((agenda ""
+;;            ((org-agenda-overriding-header "Upcoming Agenda")
+;;             ...))
+;;    ...
+;;    ))
+;;  ("o" "My Office Agenda"
+;;   ((agenda ""
+;;            ((org-agenda-overriding-header "Upcoming Agenda")
+;;             ...))
+;;    ...
+;;    ))
+;;  ("h" "My Home Agenda"
+;;   ((agenda ""
+;;            ((org-agenda-overriding-header "Upcoming Agenda")
+;;             ...))
+;;    ...
+;;    ))
+;;  ("j" "Joint Agenda"
+;;   ((agenda ""
+;;            ((org-agenda-overriding-header "Upcoming Agenda")
+;;             ...))
+;;    ...
+;;    )))
 (setq org-agenda-custom-commands
-      (quote
-       (("h" "Home Agenda" ; taglist contains :home:
-         ((agenda "" ((org-agenda-skip-function
-                       '(org-agenda-skip-entry-if 'notregexp ":home:"))))))
-         ("s" "Joint Agenda" ; taglist contains :svetlana:
-          ((agenda "" ((org-agenda-skip-function
-                        '(org-agenda-skip-entry-if 'notregexp ":svetlana:"))))))
-         ("o" "Office Agenda" ; taglist contains neither :home:, nor :svetlana:
-          ((agenda "" ((org-agenda-skip-function
-                        '(org-agenda-skip-entry-if 'regexp ":\\(home\\|svetlana\\):")))))))))
+      `(
+        ,(macroexpand
+          '(my-org-agenda-custom-template "y" "My Agenda" "all"))
+        ,(macroexpand
+          '(my-org-agenda-custom-template "o" "My Office Agenda" "office"))
+        ,(macroexpand
+          '(my-org-agenda-custom-template "h" "My Home Agenda" "home"))
+        ,(macroexpand
+          '(my-org-agenda-custom-template "j" "Joint Agenda" "sveta"))
+        ))
+
 
 ;; Allow bold/emphasis span up to 3+1 lines.
 ;; https://emacs.stackexchange.com/questions/13820/inline-verbatim-and-code-with-quotes-in-org-mode
