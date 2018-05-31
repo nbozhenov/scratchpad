7a142561cfb5042796fe3692ad534a2796bde66b
Nikolai Bozhenov <nikolai.bozhenov@gmail.com>
2018-05-31 16:27:55 +0200

    Remove hook for Info-mode (evil-collection provides better config)

--- emacs/init-2018/misc.el
+++ emacs/init-2018/misc.el
@@ -1,16 +1,4 @@
 ;;
-;; Info-mode
-;;
-(add-hook 'Info-mode-hook
- (lambda ()
-   (define-key Info-mode-map " " nil)
-   (define-key Info-mode-map "g" nil)
-   (define-key Info-mode-map "n" nil)
-   (evil-define-key 'motion Info-mode-map " " nil)
-   (evil-define-key 'motion Info-mode-map " g" 'Info-goto-node)
-   (evil-define-key 'motion Info-mode-map " n" 'Info-next)))
-
-;;
 ;; hl-line-mode
 ;;
 (add-hook 'Buffer-menu-mode-hook 'hl-line-mode)
