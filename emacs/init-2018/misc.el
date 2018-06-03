97b2624a851d5d1964c48b45dda36bdadfb369f4
Nikolai Bozhenov <nikolai.bozhenov@gmail.com>
2018-06-03 17:38:35 +0200

    Add section for appearance and GUI customization

--- emacs/init-2018/misc.el
+++ emacs/init-2018/misc.el
@@ -24,12 +24,6 @@
 ;;
 (show-paren-mode 1) ;; show matching parenthesis
 (which-key-mode 1) ;; show key bindings for incomplete commands
-(column-number-mode 1) ; display column number in status line
-(blink-cursor-mode 0) ; use non-blinking cursor
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-(scroll-bar-mode 0)
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 ;;
 ;; Hacks for windows
@@ -38,3 +32,17 @@
   (setq dired-free-space-program nil)
   (magit-auto-revert-mode 0)
   (global-magit-file-mode 0))
+
+;;
+;; Appearance and GUI customization
+;;
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(blink-cursor-mode 0)
+(column-number-mode 1) ; display column number in status line
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+;; These are two nice options for font selection. Need to make decision later.
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-9.5"))
+;; DejaVu Sans Mono-9 looks bad for special characters and when bold.
+;; (add-to-list 'default-frame-alist '(font . "Input-9"))
