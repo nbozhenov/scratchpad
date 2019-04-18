042524bb488647228fa9931f3de09ac1dbfd2d06
Nikolai Bozhenov <Nikolai.Bozhenov@gmail.com>
2019-04-18 11:36:54 -0700

    [cfg] Update MyPIM.el

--- cfg/MyPIM.el
+++ cfg/MyPIM.el
@@ -8,8 +8,14 @@
    (:startgroup "MyPIM" "p")
    ("p:Compilers" "c" "~/g/MyPIM/Compilers" "~/p/MyPIM/Compilers")
    ("p:GTD" "g" "~/g/MyPIM/GTD" "~/p/MyPIM/GTD")
-   ("p:PIM-Shteud" "i" "~/g/MyPIM/Shteud" "~/p/MyPIM/Shteud") ; ???
    ("p:PIM-Structure" "s" "~/g/MyPIM/PIM-Structure" "~/p/MyPIM/PIM-Structure")
+   (:endgroup)
+
+   (:startgroup "Intel" "i")
+   ("i:CMC" "c" "~/g/Intel/1904-VPO-CMC" "~/p/MyPIM/Intel/1904-VPO-CMC")
+   ("i:Alan" "a" "~/g/Intel/1904-Alan" "~/p/MyPIM/Intel/1904-Alan")
+   ("i:BEC" "b" "~/g/Intel/1901-BEC" "~/p/MyPIM/Intel/1901-BEC")
+   ("i:G2S" "g" "~/g/Intel/1904-G2S" "~/p/MyPIM/Intel/1904-G2S")
    (:endgroup)
 
    (:startgroup "MyQuant" "q")
